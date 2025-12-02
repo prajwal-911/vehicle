bd065a436469f26c988bb6236de5f528b6974ba8
prajwal-911 <prajwalawati101@gmail.com>
2025-12-02 12:06:57 +0530

    Create vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,16 @@
+def vehicle_details(vehicle_number, owner_name, vehicle_type, year):
+    result = (
+        f"Vehicle Number:{vehicle_number} \n"
+        f"Owner Name: {owner_name}\n"
+        f"Vehicle Type: {vehicle_type}\n"
+        f"Year of manufacture: {year}"
+    )
+    return result
+
+
+if _name_ == "_main_":
+    vehicle_number = "101"
+    owner_name = "Rohit"
+    vehicle_type = "Car"
+    year = 2022
+    print(vehicle_details(vehicle_number, owner_name, vehicle_type, year))
